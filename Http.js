70da2c677107a2b61389deebbcb134e1223673f3
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-05-10 17:01:31 +0900

    Fix: indent

--- Http.js
+++ Http.js
@@ -6,28 +6,28 @@
 * @return {Response} response レスポンスを返します 
 */ 
 function httpGet(uri, apiKey) {
-    var url = uri + "?apiKey=" + apiKey;
-      
-    logToSheet("httpGet", url);
-    return UrlFetchApp.fetch(url);
-  }
-  
-  /**
-  * HttpのPostリクエストを送信します
-  *
-  * @param {string} uri リクエストするURI
-  * @param {string} apiKey BacklogのAPIキー
-  * @param {Object<string, number>} formData 送信するフォームデータ
-  * @return {Response} response レスポンスを返します 
-  */
-  function httpPost(uri, apiKey, formData) {
-    var url = uri + "?apiKey=" + apiKey;
-    var param = {
-      "method": "post",
-      "payload" : formData
-    };
+  var url = uri + "?apiKey=" + apiKey;
     
-    logToSheet("httpPost", url);
-    return UrlFetchApp.fetch(url, param);
-  }
+  logToSheet("httpGet", url);
+  return UrlFetchApp.fetch(url);
+}
+  
+/**
+* HttpのPostリクエストを送信します
+*
+* @param {string} uri リクエストするURI
+* @param {string} apiKey BacklogのAPIキー
+* @param {Object<string, number>} formData 送信するフォームデータ
+* @return {Response} response レスポンスを返します 
+*/
+function httpPost(uri, apiKey, formData) {
+  var url = uri + "?apiKey=" + apiKey;
+  var param = {
+    "method": "post",
+    "payload" : formData
+  };
+  
+  logToSheet("httpPost", url);
+  return UrlFetchApp.fetch(url, param);
+}
   
