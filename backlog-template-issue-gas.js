09549f1ad08b60243b9bb597a6bd4fef68f08897
Shoma Nishitateno <shoma416@gmail.com>
2018-05-13 15:39:06 +0900

    Clean: move logger

--- backlog-template-issue-gas.js
+++ backlog-template-issue-gas.js
@@ -256,3 +256,18 @@ function setUserProperty(key, value) {
 function showMessage_(message) {
 	SpreadsheetApp.getActiveSpreadsheet().toast(message, SCRIPT_NAME);
 }
+
+/**
+* Log用シートにログを1行追加します
+*
+* @param {string} type 1列目に出力したい文字列
+* @param {string} value 2列目に出力したい文字列
+*/
+function logToSheet(type, value) {
+	var logMsg = new Array();
+	  
+	logMsg.push(new Date());
+	logMsg.push(type);
+	logMsg.push(value);
+	SpreadsheetApp.getActiveSpreadsheet().getSheetByName('log').appendRow(logMsg);
+  }
