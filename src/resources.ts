2aee91f4498755453009a24c3348922852d776d0
Shoma Nishitateno <shoma416@gmail.com>
2018-07-09 15:22:04 +0900

    Update: reviewed

--- src/resources.ts
+++ src/resources.ts
@@ -7,11 +7,11 @@ const messages = {
     "ja": "課題一括登録"
   },
   "title_init" : {
-    "en": "Backlog Acquire data from Backlog",
+    "en": "Backlog Acquire definitions from Backlog",
     "ja": "Backlog 定義一覧取得"
   },
   "title_run" : {
-    "en": "Backlog Execute bulk registration processing",
+    "en": "Backlog Execute bulk issue registration",
     "ja": "Backlog 課題一括登録"
   },
   "menu_step1" : {
@@ -19,7 +19,7 @@ const messages = {
     "ja": "STEP1: Backlogからデータを取得する"
   },
   "menu_step2" : {
-    "en": "STEP 2: Execute bulk registration processing",
+    "en": "STEP2: Execute bulk issue registration",
     "ja": "STEP2: 課題一括登録を実行"
   },
   "complete_init" : {
@@ -47,11 +47,11 @@ const messages = {
     "ja": "データを収集しています..."
   },
   "progress_begin" : {
-    "en": "Begin bulk registration process",
+    "en": "Started bulk issue registration",
     "ja": "一括登録を開始しました..."
   },
   "progress_end" : {
-    "en": " has been finished",
+    "en": " has finished",
     "ja": " が正常に行われました"
   }
 };
@@ -95,35 +95,35 @@ export const Message = {
   },
   VALIDATE_ERROR_LINE: (lineNumber: number, locale: Locale): string => {
     const msg = {
-      "en": `Error row ${lineNumber}: `,
+      "en": `Error occured at row ${lineNumber}: `,
       "ja": `エラー ${lineNumber} 行目: `
     }
     return msg[locale]
   },
   VALIDATE_SUMMARY_EMPTY: (locale: Locale): string => {
     const msg = {
-      "en": `'Summary' is empty`,
+      "en": `'Summary' is required`,
       "ja": `'件名' が入力されていません`
     }
     return msg[locale]
   },
   VALIDATE_ISSUE_TYPE_EMPTY: (locale: Locale): string => {
     const msg = {
-      "en": `'Issue type' is empty`,
+      "en": `'Issue type' is required`,
       "ja": `'種別名' が入力されていません`
     }
     return msg[locale]
   },
   VALIDATE_PARENT_ISSUE_KEY_NOT_FOUND: (parentIssueKey: Option<string>, locale: Locale): string => {
     const msg = {
-      "en": `The issue key [${parentIssueKey}] specified for 'parent issue' is not found`,
+      "en": `The specified 'parent issue' key [${parentIssueKey}] was not found`,
       "ja": `'親課題' に指定された課題キー [${parentIssueKey}] が見つかりません`
     }
     return msg[locale]
   },
   ALREADY_BEEN_CHILD_ISSUE: (issueKey: string, locale: Locale): string => {
     const msg = {
-      "en": `The issue '${issueKey}' has already been a child task and can not be set as a parent issue`,
+      "en": `The issue key '${issueKey}' is a child issue and can therefor not be set as a parent issue`,
       "ja": `課題 '${issueKey}' はすでに子課題となっているため、親課題として設定できません`
     }
     return msg[locale]
